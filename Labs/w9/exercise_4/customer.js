33c8de8853a5ddc300aabdf728c2879d4b9aedbe
btran369 <VuAnhQuan.Tran@georgebrown.ca>
2026-03-15 23:13:39 -0400

    w9

---
+++ Labs/w9/exercise_4/customer.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Customer = void 0;
+var Customer = /** @class */ (function () {
+    function Customer(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+    Customer.prototype.greeter = function () {
+        console.log("Hello ".concat(this.firstName, " ").concat(this.lastName));
+    };
+    ;
+    Customer.prototype.getAge = function () {
+        return this.age;
+    };
+    return Customer;
+}());
+exports.Customer = Customer;
+var customer = new Customer("John", "Smith", 25);
+customer.greeter();
